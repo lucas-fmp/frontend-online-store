4a52551e884fbf85e91fd7020995874b8a405521
João Mattedi <joaommattedi@gmail.com>
2022-07-07 17:31:22 -0300

    showing products after search

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,67 @@
 import React, { Component } from 'react';
+import ProductCard from '../components/ProductCard';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 export default class Main extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      products: [],
+      searched: false,
+      query: '',
+    };
+  }
+
+  onHandleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ query: value });
+  }
+
+  onHandleClick = async () => {
+    const { query } = this.state;
+    const data = await getProductsFromCategoryAndQuery(query);
+    this.setState({ products: data, searched: true });
+  }
+
   render() {
+    const { products, query, searched } = this.state;
+    const resultRender = searched
+      ? <div>Nenhum produto foi encontrado</div>
+      : (
+        <div
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </div>);
     return (
-      <div
-        data-testid="home-initial-message"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
+      <div>
+        <input
+          type="text"
+          data-testid="query-input"
+          onChange={ this.onHandleChange }
+          value={ query }
+        />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.onHandleClick }
+        >
+          Search
+        </button>
+        {products.length === 0
+          ? (resultRender)
+          : (
+            <div>
+              {products.map(({ id, title, price, thumbnail }) => (
+                <ProductCard
+                  key={ id }
+                  title={ title }
+                  price={ price }
+                  thumbnail={ thumbnail }
+                />))}
+            </div>
+          )}
       </div>
     );
   }
