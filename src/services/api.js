6e93a832df82c3a25059ad2f486b567d2253fe30
João Mattedi <joaommattedi@gmail.com>
2022-07-07 16:27:37 -0300

    02 requirement

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ export async function getCategories() {
   return data;
 }
 
-export async function getProductsFromCategoryAndQuery({ CATEGORYID, QUERY }) {
+export async function getProductsFromCategoryAndQuery(QUERY) {
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`);
   const data = await response.json();
   return data;
