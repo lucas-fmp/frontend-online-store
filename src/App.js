5460882002ccdc0cf267b616dca2e5efb8b8aefe
Lucas <lucasfernandomacedo13@gmail.com>
2022-07-07 17:32:01 -0300

    03 requirement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Main from './pages/Main';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Main from './pages/Main';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
+      <Link
+        to="/shopping-cart"
+        data-testid="shopping-cart-button"
+      >
+        Carrinho
+      </Link>
       <Switch>
         <Route exact path="/" component={ Main } />
+        <Route exact path="/shopping-cart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
