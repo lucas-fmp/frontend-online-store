3f892e4506b13776ef4914a91d0541449a25ac89
Cesar Xavier <cxavier.vet@gmail.com>
2022-07-12 16:21:21 -0300

    Requisito 11 feito !

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getItemFromId } from '../services/api';
+import Rating from '../components/Rating';
 
 export default class ProductPage extends Component {
   constructor() {
@@ -9,22 +10,57 @@ export default class ProductPage extends Component {
     this.state = {
       product: undefined,
       loading: true,
+      reviews: [],
     };
   }
 
   componentDidMount() {
     const { match: { params: { id } } } = this.props;
+    const getReviews = JSON.parse(localStorage.getItem('reviews'));
     getItemFromId(id)
-      .then((data) => this.setState({ product: data, loading: false }));
+      .then((data) => this.setState({ product: data,
+        loading: false,
+        reviews: getReviews || [] }));
+  }
+
+  addReview = (review) => {
+    const { reviews } = this.state;
+
+    this.setState({ reviews: [...reviews, review] }, () => {
+      const { reviews: reviewAtt } = this.state;
+      localStorage.setItem('reviews', JSON.stringify(reviewAtt));
+    });
   }
 
   render() {
-    const { product, loading } = this.state;
+    const { reviews, product, loading } = this.state;
     return (
-      <div>
-        {!loading
+      <>
+        <div>
+          {!loading
         && <h2 data-testid="product-detail-name">{product.title}</h2>}
-      </div>
+
+        </div>
+        <Rating addReview={ this.addReview } />
+        { reviews.map((review, index) => (
+          <div key={ index }>
+            <p>
+              {' '}
+              {review.email}
+              {' '}
+            </p>
+            <p>
+              {' '}
+              {review.rating}
+              {' '}
+            </p>
+            <p>
+              {' '}
+              {review.comment}
+              {' '}
+            </p>
+          </div>))}
+      </>
     );
   }
 }
